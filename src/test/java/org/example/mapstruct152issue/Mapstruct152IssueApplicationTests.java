840cf0a6db7425ed93dbaff687c3d3ae38bdd19b
julius-d <30121440+julius-d@users.noreply.github.com>
2022-09-19 16:57:13 +0200

    Initial commit

---
+++ src/test/java/org/example/mapstruct152issue/Mapstruct152IssueApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mapstruct152issue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mapstruct152IssueApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
